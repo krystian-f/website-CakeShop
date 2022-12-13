eb1930f94e229f4e5293a91d21ef69670d4d0299
krystian-f <krystianfpa@gmail.com>
2022-12-13 17:01:36 +0100

    JS slider

--- js/app.js
+++ js/app.js
@@ -11,6 +11,10 @@ from './gallery.js';
 
 // DOM
 const overlay = document.querySelector('overlay');
+const sliderBtns = document.querySelectorAll('.slider__btn')
+const sliderBtnPrev = document.querySelector('.slider__btn-prev');
+const sliderBtnNext = document.querySelector('.slider__btn-next');
+const slides =  document.querySelectorAll('.slide');
 
 // Call functions
 window.addEventListener("load", () => {
@@ -35,16 +39,55 @@ window.addEventListener("load", () => {
 
   if( subPageName === '/contact.html'){
     console.log('contact');
+    // Map
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+    L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();    
   }
   });
 
-  // Map
-  var map = L.map('map').setView([51.505, -0.09], 13);
 
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
-  
-  L.marker([51.5, -0.09]).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
+
+// Header Slider 
+let currentSlide = 0;
+let maxSlides = slides.length - 1;
+
+const displaySlide = function displayCurrentSlide(){
+  slides.forEach ((slide, index) => {
+    slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`;   
+  })
+}
+
+slides.forEach((slide, index) =>{
+  slide.style.transform = `translateX(${index * 100}%)`;
+})
+
+sliderBtns.forEach((btn)=>{
+  if(btn.classList.contains('slider__btn-prev')) {
+    btn.addEventListener('click', ()=>{
+
+      if (currentSlide === 0) {
+        currentSlide = maxSlides;
+      } else {
+        currentSlide -= 1;        
+      }
+      displaySlide()
+    }) 
+  } else if (btn.classList.contains('slider__btn-next')) {
+    btn.addEventListener('click', () =>{
+
+      if (currentSlide === maxSlides) {
+        currentSlide = 0;
+      } else {
+        currentSlide += 1;        
+      }
+      displaySlide();
+    })
+  }
+})
